a5dfc84fc4c7d28103c9efa9b7027937a3bcb261
Aydin324 <ayddin2004@gmail.com>
2024-10-04 18:47:12 +0200

    Week 1 lab tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,47 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        //Task 1
+        /*System.out.println("My name is Aydin Sulejmanovic");*/
+
+        //Task 2
+        /*System.out.println("Hello World!\n (And all people of the world)");*/
+
+        //Task 3
+        //System.out.println("Seconds in a 365-day year: " + 365*24*60*60);
+
+        //Task 4
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum: " + (a+b));*/
+
+        //Task 5
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int a = sc.nextInt();
+        if(a>0)
+            System.out.println("Number is positive!");
+        else
+            System.out.println("Number is negative!");*/
+
+        //Task 6
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b)
+            System.out.println(a+" is greater than "+ b);
+        else if(a<b)
+            System.out.println(a+" is less than "+ b);
+        else
+            System.out.println(a+" is equal to "+b);
+            }
 }
